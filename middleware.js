38c99dd5caa985755cbd2475d4baf425c70f327c
Omer Aswad <mkf.dev.umar@gmail.com>
2025-09-04 16:01:03 +0500

    tyr to fix middleware and auth

--- middleware.js
+++ middleware.js
@@ -2,8 +2,14 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req) {
-  const token = await getToken({ req, cookieName: "__Secure-next-auth.session-token", secret: process.env.AUTH_SECRET });
-  console.log("Token", token)
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+    // Let the helper choose the right cookie name:
+    secureCookie: process.env.NODE_ENV === "production",
+  });
+
+  console.log("Token", token); // Should be non-null if authenticated
   const { nextUrl } = req;
 
   const isLoggedIn = !!token;
